94300a68ea50c9a2bf92719ca43f73768415008b
Massimo Redaelli <massimo.redaelli@aia.ch>
2015-08-25 16:17:11 +0200

    Switched from regexp to parser

--- src/BibLaTeXParser.hs
+++ src/BibLaTeXParser.hs
@@ -1,11 +1,9 @@
 module BibLaTeXParser where
 
 import qualified Text.Parsec as Parsec
-
 import Text.Parsec ((<?>), (<|>))
 
 import Control.Applicative
-
 import Control.Monad.Identity (Identity)
 
 import Data.Char
@@ -14,6 +12,7 @@ import Text.Read
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+
 
 type ParserState = Map String String
 
@@ -57,15 +56,145 @@ latexCommand = do
     what <- latexSpecial Parsec.<|>  latexNormal
     return what
 
-latexSpecial = do
-    what <- Parsec.oneOf "\"'^"
-    return $ case what of
-        '^' -> "è"
+grave = chr 0x300
+acute = chr 0x301
+circumflex = chr 0x302
+tilde = chr 0x303
+macron = chr 0x304
+overline = chr 0x305
+breve = chr 0x306
+dotabove = chr 0x307
+diaeresis = chr 0x308
+hook = chr 0x309
+ring = chr 0x30a
+aacute = chr 0x30b
+caron = chr 0x30c
+vlineabove = chr 0x30d
+vvlineabove = chr 0x30e
+ggrave = chr 0x30f
+cedilla = chr 0x327
+ogonek = chr 0x328
+barred = chr 0x337
+barunder = chr 0x331 -- macron below
+dotover = chr 0x307
+dotunder = chr 0x323
+endash = chr 0x2013
+emdash = chr 0x2014
+nonbreak = chr 0x00a0
 
+
+latexSpecial :: Parsec.Parsec String ParserState String
+latexSpecial = do
+    accentSel <- Parsec.oneOf $ concat (Map.keys latexSpecials ++ Map.keys latexAccents)
+    what <- Parsec.letter Parsec.<|> Parsec.between (Parsec.char '{') (Parsec.char '}') Parsec.letter
+    let accentMod = Map.lookup [accentSel] latexSpecials
+    return $ case accentMod of
+        Just " " -> " "
+        Just accentModUTF -> what : accentModUTF
+        _ -> ['\\', accentSel] ++ [what]
+
+latexNormal :: Parsec.Parsec String ParserState String
 latexNormal = do
-    what <- idParser
-    return $ case what of
-        "S" -> "§"
+    what <- Parsec.many1 Parsec.letter
+    let res = Map.lookup what latexNormals
+    return $ case res of
+        Just s -> s
+        _ -> '\\' : what
+
+
+
+latexSpecials = Map.fromList [
+    (" ", " "),
+    ("~", "~"),
+    ("%", "%"),
+    ("{", "{"),
+    ("$", "$"),
+    ("_", "_"),
+    ("P", "¶"),
+    ("#", "#"),
+    ("&", "&"),
+    ("}", "}")
+    --("{}", "")
+    --("[^][{][}]", "^"),
+    --("[~][{][}]", "~")
+    ]
+
+latexAccents = Map.fromList [
+    ("^", [circumflex]),
+    ("`", [grave]),
+    ("'", [acute]),
+    ("\"", [diaeresis]),
+    ("~", [tilde]),
+    ("=", [macron]),
+    (".", [dotover])
+    ]
+
+latexNormals = Map.fromList [
+    ("H", [aacute]),
+    ("c", [cedilla]),
+    ("k", [ogonek]),
+    ("l", [barred]),
+    ("b", [barunder]),
+    ("d", [dotunder]),
+    ("r", [ring]),
+    ("u", [breve]),
+    ("v", [caron]),
+    ("S", "§"),
+    ("aa", "å"),
+    ("AA", "Å"),
+    ("ae", "æ"),
+    ("AE", "Æ"),
+    ("oe", "œ"),
+    ("OE", "Œ"),
+    ("o", "ø"),
+    ("O", "Ø"),
+    ("ss", "ß"),
+    ("l", "ł"),
+    ("L", "Ł"),
+    ("dh", "ð"),
+    ("DH", "Ð"),
+    ("dj", "đ"),
+    ("DJ", "Ð"),
+    ("ng", "ŋ"),
+    ("NG", "Ŋ"),
+    ("dag", "†"),
+    ("ddag", "‡"),
+    ("pounds", "£"),
+    ("textasciicircum", "^"),
+    ("textasciitilde", "~"),
+    ("textasteriskcentered", "∗"),
+    ("textbackslash", "\\"),
+    ("textbar", "|"),
+    ("textbraceleft", "{"),
+    ("textbraceright", "}"),
+    ("textbullet", "•"),
+    ("textcopyright", "©"),
+    ("textdagger", "†"),
+    ("textdaggerdbl", "‡"),
+    ("textdollar", "$"),
+    ("textellipsis", "…"),
+    ("textemdash", [emdash]),
+    ("textendash", [endash]),
+    ("textexclamdown", "¡"),
+    ("textgreater", ">"),
+    ("textless", "<"),
+    ("textordfeminine", "ª"),
+    ("textordmasculine", "º"),
+    ("textparagraph", "¶"),
+    ("textperiodcentered", "·"),
+    ("textquestiondown", "¿"),
+    ("textquotedblleft", "“"),
+    ("textquotedblright", "”"),
+    ("textquoteleft", "‘"),
+    ("textquoteright", "’"),
+    ("textregistered", "®"),
+    ("textsection", "§"),
+    ("textsterling", "£"),
+    ("texttrademark", "™"),
+    ("textunderscore", "_"),
+    ("textvisiblespace", "␣")
+    ]
+
 
 
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,24 @@
 module Main where
 
-import BibLaTeXParser (parseFile, parseEntry, toBibTeX)
-import Accentuate
+import BibLaTeXParser (parseFile, parseEntry, bibtexString, parse)
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import System.IO
 
 main = do
-   putStrLn $ latexToUnicode "This \\^\\i is an \\important example of the many text commands of LaTeX.\
+   hSetEncoding stdout utf8
+   let result = parse (bibtexString False) Map.empty  "This \\^i is an \\important example of the many text commands of LaTeX. \
     \In Italian we can use for instance \\'e, as in \\textquotedblleft perch\\'e\\textquotedblright\
-    \, \\`{e}, \\'o, \\`o, \\`a, \\`\\i and \\`u, but not much more than that \\textendash\\ although you could \
-    \go a bit archaic, and use also \\^\\i for some endings. In German they have all their crazy \\\"o- \
+    \, \\`{e}, \\'o, \\`o, \\`a, \\`i and \\`u, but not much more than that \\textendash\\ although you could \
+    \go a bit archaic, and use also \\^i for some endings. In German they have all their crazy \\\"o- \
     \and \\\"{a}-, \\\"u's, and even good old \\ss. For instance S\\\"u\\ss." {-
    s <- readFile "biblatext-test.bib"
    let result = parseFile s-- "@string{ciao = {ciao}} @Article{key03, author= ciao, title=\"A d title\",       }  @article{rt, ar={k{\"}r}, pr=\"f{g}g{f{{}ff{}}}d\",}Hello @article{test, year=2015, pippo=\"ciao\"} "
-   --let result = parseFile "  @Article{key03, author={c}, title=\"A {bunch {of} braces {in}} title\"    } "
+   --let result = parseFile "  @Article{key03, author={c}, title=\"A {bunch {of} braces {in}} title\"    } "-}
    case result of
        Right v -> do
-            let res = map toBibTeX v
-            putStrLn $ show res
+            --let res = map toBibTeX v
+            putStrLn v
        Left err -> putStrLn ("whoops, error: "++show err)
--}
